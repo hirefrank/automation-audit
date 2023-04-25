df617a9b9a0b661fcfc5165b36227d9e794cddb0
Frank Harris <fcharris@gmail.com>
2023-04-24 21:30:59 -0400

    updated folder strucure, removed submodule

---
+++ src/zapier.ts
@@ -0,0 +1,15 @@
+// For more information, see https://crawlee.dev/
+import { PlaywrightCrawler, ProxyConfiguration, Dataset } from 'crawlee';
+import { router } from './routes/zapier.js';
+
+const startUrls = ['https://zapier.com/apps'];
+
+const crawler = new PlaywrightCrawler({
+    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+    requestHandler: router,
+    headless: true
+});
+
+await crawler.run(startUrls);
+await Dataset.exportToCSV('zapier');
+
